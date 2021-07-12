8be35ff36bc993099aa45673d4d07b4a8864d8cf
hexadoo <hexadoo@hotmail.com>
2021-07-12 12:43:59 -0400

    Update L298N.py

--- L298N.py
+++ L298N.py
@@ -1,14 +1,14 @@
 # L298 code Inspired by: Olivier Lenoir - olivier.len02@gmail.com
 
 # Author: Hexadoo - hexadoo1@gmail.com
-# Created: 2021-07-04 11:08
+# Created: 2021-07-04 20:08
 # Project: L298 Dual H-bridge, MicroPython, ESP32
 # Description: Set_Speed is from 0-100% 
 
 from machine import Pin, PWM
 from utime import sleep
 
-class L298(object):
+class L298N:
     def __init__(self, en, in1, in2, freq=1000):
         self.freq = freq
         self.speed = 0
@@ -36,16 +36,15 @@ class L298(object):
     def set_speed(self, speed):
         speed = int(speed * 1024 / 100)
         self.speed = min(1023, max(0, speed))
-    
-    # direction 1 = foward 0 = reverse
+
     def move_to_stop(self,direction,speed,time):
-        self.set_speed(speed)
+        
         if direction == 1:
             self.forward()
-        elif direstion == 0:
+        elif direction == 0:
             self.reverse()
         else:
             self.stop()
-        
+        self.set_speed(speed)
         sleep(time)
         self.stop()
